839c4b0a8cdd91e054725f15a00625da5cc4e5f3
kandivalasa prasad <kandivalasa.prasad97@gmail.com>
2021-11-13 16:16:00 +0530

    Added: using interface

---
+++ src/com/bridgelabz/EmployeeWageBuilder/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.bridgelab.EmployeePayroll;
+
+public interface EmployeeService {
+	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfworkingDays, int maxHoursPerMonth);
+
+	public void computeEmpWage();
+
+	int getTotalWage(String company);
+
+}
